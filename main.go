47febb029276624dabd99dd71903711d19553402
Pulak Kanti Bhowmick <pulak@appscode.com>
2021-11-18 15:42:55 +0600

    Add pods cpu calculation example
    Signed-off-by: Pulak Kanti Bhowmick <pulak@appscode.com>

--- main.go
+++ main.go
@@ -4,30 +4,35 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
+	"math"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"k8s.io/client-go/util/homedir"
+
 	promapi "github.com/prometheus/client_golang/api"
 	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	"log"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"time"
 )
 
-func getKubeClient() (kubernetes.Interface,error) {
-	var kubeconfig *string
+var kubeconfig *string
+
+func init() {
 	if home := homedir.HomeDir(); home != "" {
-		// fmt.Println(home)
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
-	//fmt.Println(*kubeconfig)
 	flag.Parse()
+}
+
+func getKubeClient() (kubernetes.Interface, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		return nil, err
@@ -52,7 +57,7 @@ func getStatefulSetCPU(ns, name string) (*float64, error) {
 		return nil, err
 	}
 
-	pods := make([]string,0)
+	pods := make([]string, 0)
 
 	for _, pod := range podList.Items {
 		if len(pod.OwnerReferences) > 0 {
@@ -62,15 +67,24 @@ func getStatefulSetCPU(ns, name string) (*float64, error) {
 			}
 		}
 	}
-	podQuery := strings.Join(pods, "|")
-	val, err := getPromQueryResult(ns, podQuery)
+
+	lcp := LCP(pods)
+	var podPromRegex string
+
+	if lcp == "" {
+		podPromRegex = strings.Join(pods, "|")
+	} else {
+		podPromRegex = fmt.Sprintf("%s.*", lcp)
+	}
+
+	val, err := getPromQueryResult(ns, podPromRegex)
 	if err != nil {
 		return nil, err
 	}
 	return val, nil
 }
 
-func getPromQueryResult(ns, podQuery string) (*float64,error) {
+func getPromQueryResult(ns, podQuery string) (*float64, error) {
 	client, err := promapi.NewClient(promapi.Config{
 		Address: "http://127.0.0.1:9090/",
 	})
@@ -79,7 +93,7 @@ func getPromQueryResult(ns, podQuery string) (*float64,error) {
 	}
 	promClient := promv1.NewAPI(client)
 
-	promQuery := fmt.Sprintf(`node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace="%s", pod=~"%s"}`,ns, podQuery)
+	promQuery := fmt.Sprintf(`node_namespace_pod_container:container_cpu_usage_seconds_total:sum_irate{namespace="%s", pod=~"%s"}`, ns, podQuery)
 	fmt.Println(promQuery)
 	val, warn, err := promClient.Query(context.Background(), promQuery, time.Now())
 	if err != nil {
@@ -112,14 +126,90 @@ func getPromQueryResult(ns, podQuery string) (*float64,error) {
 	return &cpu, nil
 }
 
-func main()  {
-	val, err := getStatefulSetCPU("demo", "pg-demo")
+func LCP(strs []string) string {
+	minLen := math.MaxInt32
+	for _, s := range strs {
+		minLen = minINT(len(s), minLen)
+	}
+
+	lcpLen := 0
+
+	for i := 0; i < minLen; i++ {
+		ok := true
+		for j := 1; j < len(strs); j++ {
+			if strs[0][i] != strs[j][i] {
+				ok = false
+				break
+			}
+		}
+		if !ok {
+			break
+		}
+		lcpLen += 1
+	}
+	if lcpLen == 0 {
+		return ""
+	}
+	return strs[0][:lcpLen]
+}
+
+func minINT(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func getPodsCPU(ns string, selector map[string]string) (*float64, error) {
+	kc, err := getKubeClient()
+	if err != nil {
+		return nil, err
+	}
+	podList, err := kc.CoreV1().Pods(ns).List(context.Background(), metav1.ListOptions{
+		LabelSelector: labels.Set(selector).String(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	pods := make([]string, 0)
+
+	for _, pod := range podList.Items {
+		pods = append(pods, pod.Name)
+	}
+
+	lcp := LCP(pods)
+	var podPromRegex string
+
+	if lcp == "" {
+		podPromRegex = strings.Join(pods, "|")
+	} else {
+		podPromRegex = fmt.Sprintf("%s.*", lcp)
+	}
+
+	val, err := getPromQueryResult(ns, podPromRegex)
+	if err != nil {
+		return nil, err
+	}
+	return val, nil
+}
+
+func main() {
+	val, err := getStatefulSetCPU("demo", "mg-sh-shard0")
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	fmt.Println("CPU Usage: ", *val)
+
+	selector := make(map[string]string)
+	selector["app.kubernetes.io/instance"] = "mg-sh"
+	selector["app.kubernetes.io/managed-by"] = "kubedb.com"
+	selector["app.kubernetes.io/name"] = "mongodbs.kubedb.com"
+
+	val, err = getPodsCPU("demo", selector)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("CPU Usage: ", *val)
+
 }
-
-
-
